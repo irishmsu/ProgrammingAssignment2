5c3eb38b8de3d3c31e568963b85f0fe5bb12df7c
irishmsu <shannonhall79@gmail.com>
2015-12-27 15:31:19 -0600

    Commet cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  InverseMatrix <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    InverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) InverseMatrix <<- inverse
+  getInverse <- function() InverseMatrix 
+  list(set = set, get = get, setInverse = setInverse, getInverse = 	  	getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+  ## Return a matrix that is the inverse of 'x'
+  InverseX <- x$getInverse()
+  if(!is.null(InverseX))
+  {
+    message("getting cached data")
+    return(InverseX)
+  }
+  
+  data <- x$get()
+  InverseX <- solve(data, ...)
+  x$setInverse(InverseX)
+  InverseX 
 }
